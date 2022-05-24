8e93b1fdc9bbaa128bcf3578e9f94e22f034e2aa
Josias Santos <josiassantos1577@gmail.com>
2022-05-23 22:37:05 -0300

    menu

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
 function onScroll() {
-  console.log("Função Chamada");
+  // console.log("Função Chamada");
+  if (scrollY > 0) {    
+  navigation.classList.add('scroll')
+  } else {
+    navigation.classList.remove('scroll')
+  }
+
 }
